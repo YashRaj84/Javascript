e863480ea63be2252f814132cbe25fcf7d2fe1c4
Yash Raj <149456901+YashRaj84@users.noreply.github.com>
2025-02-24 15:25:40 +0000

    updated arrays

--- 02_basics/03_functions.js
+++ 02_basics/03_functions.js
@@ -23,5 +23,29 @@ function loginUserMessage(username) {
     }
     return `Hello, ${username}. You are successfully logged in.`
 }
-console.log(loginUserMessage("Yash Raj"))
-console.log(loginUserMessage())
+// console.log(loginUserMessage("Yash Raj"))
+// console.log(loginUserMessage())
+
+function calculateCartPrice(...num) {
+    return num
+}
+// console.log(calculateCartPrice(200,434,998))
+
+const user = {
+    username: "yash",
+    age: 19
+}
+function handleObject(anyObj) {
+    console.log(`Username is ${anyObj.username} and age is ${anyObj.age}`)
+}
+// handleObject(user)
+
+//We can also pass an object directly
+// handleObject({username: "Yash", age: 19})
+
+const myArr = ["Yash Raj", 19, "LPU"]
+function userDetails(anyArr) {
+    console.log(`${anyArr[0]} is ${anyArr[1]} years old and is a student of ${anyArr[2]}.`)
+}
+userDetails(myArr)
+userDetails(["Anmol Vats", 21, "Amity University"])
